e44b819e233dbcca2f9fb30ae1a5ebd0f5f04d71
Alex Robson <asrobson@gmail.com>
2015-06-11 08:31:58 -0500

    Bug fix - add missing require statements. Add metric name support to instrument calls and telemetry. Support byte conversions.

--- spec/behavior/converter.spec.js
+++ spec/behavior/converter.spec.js
@@ -3,60 +3,107 @@ var convert = require( '../../convert' );
 console.log( convert );
 
 describe( 'Converter', function() {
+	describe( 'when converting time units', function() {
+		it( 'should convert seconds to milliseconds', function() {
+			convert( 1, 's', 'ms' ).should.equal( 1000 );
+		} );
 
-	it( 'should convert seconds to milliseconds', function() {
-		convert( 1, 's', 'ms' ).should.equal( 1000 );
+		it( 'should convert seconds to microseconds', function() {
+			convert( 1, 's', 'us' ).should.equal( 1000000 );
+		} );
+
+		it( 'should convert seconds to nanoseconds', function() {
+			convert( 1, 's', 'ns' ).should.equal( 1000000000 );
+		} );
+
+		it( 'should convert milliseconds to seconds', function() {
+			convert( 1000, 'ms', 's' ).should.equal( 1 );
+		} );
+
+
+		it( 'should convert milliseconds to microseconds', function() {
+			convert( 0.001, 'ms', 'us' ).should.equal( 1 );
+		} );
+
+		it( 'should convert milliseconds to nanoseconds', function() {
+			convert( 0.000001, 'ms', 'ns' ).should.equal( 1 );
+		} );
+
+		it( 'should convert microseconds to milliseconds', function() {
+			convert( 1, 'us', 'ms' ).should.equal( 0.001 );
+		} );
+
+		it( 'should convert microseconds to seconds', function() {
+			convert( 1000000, 'us', 's' ).should.equal( 1 );
+		} );
+
+		it( 'should convert microseconds to nanoseconds', function() {
+			convert( 0.001, 'us', 'ns' ).should.equal( 1 );
+		} );
+
+		it( 'should convert nanoseconds to milliseconds', function() {
+			convert( 1000000, 'ns', 'ms' ).should.equal( 1 );
+		} );
+
+		it( 'should convert nanoseconds to microseconds', function() {
+			convert( 1000, 'ns', 'us' ).should.equal( 1 );
+		} );
+
+		it( 'should convert nanoseconds to seconds', function() {
+			convert( 1000000000, 'ns', 's' ).should.equal( 1 );
+		} );
+
+		it( 'should maintain like units', function() {
+			convert( 1, 's', 's' ).should.equal( 1 );
+			convert( 1, 'ms', 'ms' ).should.equal( 1 );
+			convert( 1, 'us', 'us' ).should.equal( 1 );
+			convert( 1, 'ns', 'ns' ).should.equal( 1 );
+		} );
 	} );
 
-	it( 'should convert seconds to microseconds', function() {
-		convert( 1, 's', 'us' ).should.equal( 1000000 );
-	} );
+	describe( 'when converting time units', function() {
+		it( 'should convert bytes to kilobytes', function() {
+			convert( 1024, 'b', 'kb' ).should.equal( 1 );
+		} );
 
-	it( 'should convert seconds to nanoseconds', function() {
-		convert( 1, 's', 'ns' ).should.equal( 1000000000 );
-	} );
+		it( 'should convert bytes to megabytes', function() {
+			convert( 1048576, 'b', 'mb' ).should.equal( 1 );
+		} );
 
-	it( 'should convert milliseconds to seconds', function() {
-		convert( 1000, 'ms', 's' ).should.equal( 1 );
-	} );
+		it( 'should convert bytes to gigabytes', function() {
+			convert( 5368709120, 'b', 'gb' ).should.equal( 5 );
+		} );
 
+		it( 'should convert bytes to terabytes', function() {
+			convert( 7696581394432, 'b', 'tb' ).should.equal( 7 );
+		} );
 
-	it( 'should convert milliseconds to microseconds', function() {
-		convert( 0.001, 'ms', 'us' ).should.equal( 1 );
-	} );
+		it( 'should convert terabytes to gigabytes', function() {
+			convert( 3, 'tb', 'gb' ).should.equal( 3072 );
+		} );
 
-	it( 'should convert milliseconds to nanoseconds', function() {
-		convert( 0.000001, 'ms', 'ns' ).should.equal( 1 );
-	} );
+		it( 'should convert terabytes to megabytes', function() {
+			convert( 4, 'tb', 'mb' ).should.equal( 4194304 );
+		} );
 
-	it( 'should convert microseconds to milliseconds', function() {
-		convert( 1, 'us', 'ms' ).should.equal( 0.001 );
-	} );
+		it( 'should convert terabytes to kilobytes', function() {
+			convert( 10, 'tb', 'kb' ).should.equal( 10737418240 );
+		} );
 
-	it( 'should convert microseconds to seconds', function() {
-		convert( 1000000, 'us', 's' ).should.equal( 1 );
-	} );
+		it( 'should convert gigbytes to megabytes', function() {
+			convert( 6, 'gb', 'mb' ).should.equal( 6144 );
+		} );
 
-	it( 'should convert microseconds to nanoseconds', function() {
-		convert( 0.001, 'us', 'ns' ).should.equal( 1 );
-	} );
+		it( 'should convert gigbytes to kilobytes', function() {
+			convert( 8, 'gb', 'kb' ).should.equal( 8388608 );
+		} );
 
-	it( 'should convert nanoseconds to milliseconds', function() {
-		convert( 1000000, 'ns', 'ms' ).should.equal( 1 );
-	} );
-
-	it( 'should convert nanoseconds to microseconds', function() {
-		convert( 1000, 'ns', 'us' ).should.equal( 1 );
-	} );
-
-	it( 'should convert nanoseconds to seconds', function() {
-		convert( 1000000000, 'ns', 's' ).should.equal( 1 );
-	} );
-
-	it( 'should maintain like units', function() {
-		convert( 1, 's', 's' ).should.equal( 1 );
-		convert( 1, 'ms', 'ms' ).should.equal( 1 );
-		convert( 1, 'us', 'us' ).should.equal( 1 );
-		convert( 1, 'ns', 'ns' ).should.equal( 1 );
+		it( 'should maintain like units', function() {
+			convert( 1, 'b', 'b' ).should.equal( 1 );
+			convert( 1, 'kb', 'kb' ).should.equal( 1 );
+			convert( 1, 'mb', 'mb' ).should.equal( 1 );
+			convert( 1, 'gb', 'gb' ).should.equal( 1 );
+			convert( 1, 'tb', 'tb' ).should.equal( 1 );
+		} );
 	} );
 } );
--- src/converter.js
+++ src/converter.js
@@ -1,13 +1,36 @@
+var _ = require( 'lodash' );
+var timeLookup = [ 1, 1000, 1000000, 1000000000 ];
+var timeUnits = [ 'NS', 'US', 'MS', 'S' ];
 
-var lookup = [ 1, 1000, 1000000, 1000000000 ];
-var units = [ 'ns', 'us', 'ms', 's' ];
+var byteLookup = [ 1, 1024, 1048576, 1073741824, 1099511627776 ]
+var byteUnits = [ 'B', 'KB', 'MB', 'GB', 'TB' ];
+
+function convertTime( value, sourceUnits, destinationUnits ) {
+	var sourceIndex = _.indexOf( timeUnits, sourceUnits );
+	var destinationIndex = _.indexOf( timeUnits, destinationUnits );
+	var index = Math.abs( sourceIndex - destinationIndex );
+	var factor = timeLookup[ index ];
+	return sourceIndex > destinationIndex ? value * factor : value / factor;
+}
+
+function convertBytes( value, sourceUnits, destinationUnits ) {
+	var sourceIndex = _.indexOf( byteUnits, sourceUnits );
+	var destinationIndex = _.indexOf( byteUnits, destinationUnits );
+	var index = Math.abs( sourceIndex - destinationIndex );
+	var factor = byteLookup[ index ];
+	return sourceIndex > destinationIndex ? value * factor : value / factor;
+}
 
 function convert( value, sourceUnits, destinationUnits ) {
-	var sourceIndex = _.indexOf( units, sourceUnits );
-	var destinationIndex = _.indexOf( units, destinationUnits );
-	var index = Math.abs( sourceIndex - destinationIndex );
-	var factor = lookup[ index ];
-	return sourceIndex > destinationIndex ? value * factor : value / factor;
+	sourceUnits = sourceUnits.toUpperCase()
+	destinationUnits = destinationUnits.toUpperCase();
+	if( _.contains( timeUnits, sourceUnits ) ) {
+		return convertTime( value, sourceUnits, destinationUnits );
+	} else if( _.contains( byteUnits, sourceUnits ) ) {
+		return convertBytes( value, sourceUnits, destinationUnits );
+	} else {
+		throw new Error( 'Metronic converter only supports conversion between time or byte measurements' );
+	}
 }
 
 module.exports = convert;
